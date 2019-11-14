071d8662688f1da93c1d448d3e40e8c4ed9d1da1
Aika Sat <aikasat95@gmail.com>
2019-11-15 03:48:58 +0600

    added css

--- src/App.js
+++ src/App.js
@@ -8,9 +8,9 @@ import './App.css';
 class App extends Component {
   state = {
     tasks : [
-      {name: 'Clean my room', id: nanoid()},
-      {name: 'Do the dishes', id: nanoid()},
-      {name: 'Sing a song', id: nanoid()},
+      {name: 'Clean my room', completed: false, id: nanoid()},
+      {name: 'Do the dishes', completed: false, id: nanoid()},
+      {name: 'Sing a song', completed: true, id: nanoid()},
     ],
     currentTask : '',
   };
@@ -19,27 +19,43 @@ class App extends Component {
     e.preventDefault();
 
     if(this.validateForm()) {
-      const tasks = [...this.state.tasks];
-      tasks.push({name: this.state.currentTask, id: nanoid()});
+      const tasks = [...this.state.tasks, {name: this.state.currentTask, completed: false, id: nanoid()}];
       this.setState({tasks});
+
+      let currentTask = this.state.currentTask;
+      currentTask = '';
+      this.setState({currentTask: currentTask});
     }
   };
+
   validateForm =()=> (this.state.currentTask.trim());
 
   handleInput = e => {
     this.setState({currentTask: e.target.value});
   };
+
   removeTask = id => {
     const taskIndex = this.state.tasks.findIndex(p => p.id === id);
     const tasks = [...this.state.tasks];
     tasks.splice(taskIndex, 1);
     this.setState({tasks});
   };
+
+  handleCompleteTasks = id => {
+    const taskIndex = this.state.tasks.findIndex(p => p.id === id);
+    const tasks = [...this.state.tasks];
+    this.setState({isChecked: !this.state.isChecked});
+    tasks[taskIndex].completed = !tasks[taskIndex].completed;
+    this.setState({tasks});
+  };
+
   render() {
     const tasks = this.state.tasks.map((task) => (
       <Task name={task.name}
+            completed={task.completed}
             key={task.id}
             onClick={()=>this.removeTask(task.id)}
+            onChange = {()=>this.handleCompleteTasks(task.id)}
           />
       ));
 
@@ -51,7 +67,7 @@ class App extends Component {
           addTask={(e)=>this.addTask(e)}
           onChange={(e)=>this.handleInput(e)}
           isValid = {isValid}
-        />
+          task = {this.state.currentTask}/>
         {tasks}
       </div>
     )
